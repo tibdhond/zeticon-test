eba250d48b80137ffea5cdfb6402bb9549a5f9e8
tibdhond <tibo.dhondt@telenet.be>
2022-04-10 17:28:38 +0200

    fix browse > home bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { LocationStrategy } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { BrowseService } from '../browse.service';
 import { RecordItem } from '../models/record';
 import { RecordsService } from '../records.service';
@@ -18,15 +18,19 @@ export class HomeComponent implements OnInit {
   constructor(
     private recordsService: RecordsService,
     private browseService: BrowseService,
-    private location: LocationStrategy
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.location.onPopState(() => this.browseService.emitIndexUpdate(-1));
+    this.route.url.subscribe(() => this.browseService.emitIndexUpdate(-1));
     this.browseService.previewIndexListener().subscribe(index => this.itemPreviewIndex = index);
 
     const result = this.recordsService.getRecords();
     this.records = result.Results;
     this.total = result.NrOfResults;
+  }
+
+  ngOnChanges() {
+    alert("test");
   }
 }
